cd72e16f9377674ab41ade03881c19be07755ad2
João Pedro Vieira <jp_vieira@aol.com>
2022-08-29 12:17:38 -0300

    ProtectedRoute params type fix

--- src/Components/Helper/ProtectedRoute.tsx
+++ src/Components/Helper/ProtectedRoute.tsx
@@ -2,12 +2,11 @@ import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { UserContext } from '../../UserContext'
 
-const ProtectedRoute = (
-  params: React.DetailedHTMLProps<
-    React.HTMLAttributes<HTMLDivElement>,
-    HTMLDivElement
-  >
-) => {
+interface IProtectedRoute {
+  children: JSX.Element
+}
+
+const ProtectedRoute = (params: IProtectedRoute) => {
   const context = React.useContext(UserContext)
 
   if (context?.login === true) return <>{params.children}</>
